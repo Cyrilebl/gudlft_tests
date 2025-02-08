979209e0995eedf78fe0f86c27b08ecf499bdfac
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-02-08 20:43:16 +0100

    clubs can use only available points

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import json
 from flask import (
     Blueprint,
     current_app,
@@ -51,16 +52,52 @@ def book(competition, club):
         return render_template("welcome.html", club=club, competitions=competitions)
 
 
-# @main.route("/purchasePlaces", methods=["POST"])
-# def purchasePlaces():
-#     competition = [c for c in competitions if c["name"] == request.form["competition"]][
-#         0
-#     ]
-#     club = [c for c in clubs if c["name"] == request.form["club"]][0]
-#     placesRequired = int(request.form["places"])
-#     competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
-#     flash("Great-booking complete!")
-#     return render_template("welcome.html", club=club, competitions=competitions)
+@main.route("/purchasePlaces", methods=["POST"])
+def purchasePlaces():
+    clubs = current_app.clubs
+    competitions = current_app.competitions
+
+    competition = [c for c in competitions if c["name"] == request.form["competition"]][
+        0
+    ]
+    club = [c for c in clubs if c["name"] == request.form["club"]][0]
+
+    places_required = request.form["places"]
+    if not places_required:
+        flash("Please enter a number")
+        return redirect(request.referrer)
+
+    places_required = int(places_required)
+    club_points = int(club["points"])
+    places_available = int(competition["numberOfPlaces"])
+
+    if places_required <= 0:
+        flash("Please enter a number greater than zero")
+        return redirect(request.referrer)
+
+    elif places_required > club_points:
+        point_label = "point" if club_points == 1 else "points"
+        flash(f"Sorry, your club has {club_points} {point_label} left")
+        return redirect(request.referrer)
+
+    elif places_required > places_available:
+        places_label = "place" if club_points == 1 else "places"
+        flash(f"Sorry, only {places_available} {places_label} left")
+        return redirect(request.referrer)
+
+    club["points"] = str(club_points - places_required)
+    competition["numberOfPlaces"] = str(places_available - places_required)
+
+    # Update club points
+    with open("data/clubs.json", "w") as file:
+        json.dump({"clubs": clubs}, file)
+
+    # Update competition places
+    with open("data/competitions.json", "w") as file:
+        json.dump({"competitions": competitions}, file)
+
+    flash("Great - booking complete!")
+    return render_template("welcome.html", club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
