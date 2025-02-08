91fc7b763502c4e4ff8bfe28a8aa8be9e55d3bbc
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-02-08 14:45:15 +0100

    change config_class

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,15 @@
 from flask import Flask
-from config import Config
+from config import DevelopmentConfig
 
 from .utils import load_clubs, load_competitions
 from .routes import main
 
 
-def create_app(test_config=None):
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
 
     # Load configuration
-    app.config.from_object(Config)
-
-    if test_config:
-        app.config.update(test_config)
+    app.config.from_object(config_class)
 
     # Load data
     app.clubs = load_clubs()
