88b797b168a65732a5421e15b00241e1141d43a7
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-02-10 12:42:37 +0100

    Add tests for booking places in past competition

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,15 @@ def client():
 def setup(client, monkeypatch):
     """Set up fake data"""
     fake_club = [{"name": "Club Test", "email": "test@example.com", "points": "10"}]
-    fake_competition = [{"name": "Competition Test", "numberOfPlaces": "5"}]
+    fake_competition = [
+        {
+            "name": "Competition Test",
+            "date": "2027-01-01 10:00:00",
+            "numberOfPlaces": "5",
+        }
+    ]
     monkeypatch.setattr(client.application, "clubs", fake_club)
     monkeypatch.setattr(client.application, "competitions", fake_competition)
+
+    # Default values to avoid repetitions
+    client.common_data = {"club": "Club Test", "competition": "Competition Test"}
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,5 @@
+from freezegun import freeze_time
+
 # Index
 
 
@@ -39,7 +41,7 @@ def test_purchase_places_with_valid_number(client):
     """Test with a club having enough points and a competition with enough places."""
     response = client.post(
         "/purchasePlaces",
-        data={"club": "Club Test", "competition": "Competition Test", "places": "3"},
+        data={**client.common_data, "places": "3"},
     )
     assert response.status_code == 200
     assert b"Great - booking complete!" in response.data
@@ -56,7 +58,7 @@ def test_purchase_places_with_empty_number(client):
     """Test with no value provided"""
     response = client.post(
         "/purchasePlaces",
-        data={"club": "Club Test", "competition": "Competition Test", "places": ""},
+        data={**client.common_data, "places": ""},
     )
     assert response.status_code == 200
     assert b"Please enter a number" in response.data
@@ -66,7 +68,7 @@ def test_purchase_places_with_negative_number(client):
     """Test with no negative number"""
     response = client.post(
         "/purchasePlaces",
-        data={"club": "Club Test", "competition": "Competition Test", "places": "-2"},
+        data={**client.common_data, "places": "-2"},
     )
     assert response.status_code == 200
     assert b"Please enter a number greater than zero" in response.data
@@ -76,17 +78,34 @@ def test_purchase_places_with_zero(client):
     """Test with zero"""
     response = client.post(
         "/purchasePlaces",
-        data={"club": "Club Test", "competition": "Competition Test", "places": "0"},
+        data={**client.common_data, "places": "0"},
     )
     assert response.status_code == 200
     assert b"Please enter a number greater than zero" in response.data
+
+
+@freeze_time("2030-01-01 12:00:00")
+def test_purchase_in_past_competition(client):
+    """Test booking places for a past competition"""
+    response = client.post(
+        "/purchasePlaces",
+        data={
+            **client.common_data,
+            "places": 2,
+        },
+    )
+    assert response.status_code == 200
+    assert (
+        b"This competition already took place on January 01 at 10:00 AM"
+        in response.data
+    )
 
 
 def test_purchase_more_than_12_places(client):
     """Test to request more than 12 club points"""
     response = client.post(
         "/purchasePlaces",
-        data={"club": "Club Test", "competition": "Competition Test", "places": "13"},
+        data={**client.common_data, "places": "13"},
     )
     assert response.status_code == 200
     assert b"You cannot book more than 12 places" in response.data
@@ -96,7 +115,7 @@ def test_purchase_places_request_above_club_points(client):
     """Test to request more club points than are available"""
     response = client.post(
         "/purchasePlaces",
-        data={"club": "Club Test", "competition": "Competition Test", "places": "11"},
+        data={**client.common_data, "places": "11"},
     )
     assert response.status_code == 200
     assert b"Sorry, your club has 10 points left" in response.data
@@ -106,7 +125,7 @@ def test_purchase_places_request_above_competition_places(client):
     """Test to request more competition places than are available"""
     response = client.post(
         "/purchasePlaces",
-        data={"club": "Club Test", "competition": "Competition Test", "places": "6"},
+        data={**client.common_data, "places": "6"},
     )
     assert response.status_code == 200
     assert b"Sorry, the competition has 5 places left" in response.data
