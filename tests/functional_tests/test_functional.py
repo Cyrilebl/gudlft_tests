02c5a17fd7c668b40dc6d0ea27412401fd718ca0
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-02-19 22:52:58 +0100

    add inject_test_data

--- tests/functional_tests/test_functional.py
+++ tests/functional_tests/test_functional.py
@@ -1,14 +1,27 @@
-import pytest
-import server
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
 
 URL = "http://127.0.0.1:5000"
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+
+def inject_test_data():
+    """Send data to Flask server"""
+    test_data = {
+        "clubs": [{"name": "Club Test", "email": "test@example.com", "points": "10"}],
+        "competitions": [
+            {
+                "name": "Competition Test",
+                "date": "2027-01-01 10:00:00",
+                "numberOfPlaces": "5",
+            }
+        ],
+    }
+    response = requests.post(f"{URL}/set-test-data", json=test_data)
 
 
 def visit_homepage():
@@ -24,19 +37,15 @@ def navigate_to_club_board():
 def login(email):
     email_field = driver.find_element(By.NAME, "email")
     email_field.send_keys(email)
-    print("Email saisi:", email)
     form = driver.find_element(By.TAG_NAME, "form")
     form.submit()
-    print("URL après connexion:", driver.current_url)
 
 
 def book_places_for_competition():
     booking_link = driver.find_element(By.LINK_TEXT, "Book Places")
     booking_link.click()
-
     places_field = driver.find_element(By.NAME, "places")
     places_field.send_keys("3")
-    time.sleep(2)
     form = driver.find_element(By.TAG_NAME, "form")
     form.submit()
 
@@ -46,26 +55,11 @@ def logout():
     logout_link.click()
 
 
-@pytest.fixture
-def setup(monkeypatch):
-    fake_club = [{"name": "Club Test", "email": "test@example.com", "points": "10"}]
-    fake_competition = [
-        {
-            "name": "Competition Test",
-            "date": "2027-01-01 10:00:00",
-            "numberOfPlaces": "5",
-        }
-    ]
-    monkeypatch.setattr(server, "clubs", fake_club)
-    monkeypatch.setattr(server, "competitions", fake_competition)
-
-
 def test_run_functional():
+    inject_test_data()
     visit_homepage()
     navigate_to_club_board()
     login("test@example.com")
-    print("Clubs mockés:", server.clubs)
-    print("Comp mockés:", server.competitions)
     navigate_to_club_board()
     book_places_for_competition()
     logout()
