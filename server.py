20e45d54c674708f7cba3fccb1d887647b09605f
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-02-26 17:25:34 +0100

    refactor with get_data and is_valid

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
-import os
 import json
+import os
 import secrets
 from datetime import datetime
+
 from flask import (
     Flask,
+    flash,
+    jsonify,
+    redirect,
     render_template,
     request,
-    redirect,
-    flash,
-    url_for,
     session,
-    jsonify,
+    url_for,
 )
 
 app = Flask(__name__)
@@ -69,75 +70,74 @@ def home():
     return render_template("welcome.html", club=club, competitions=competitions)
 
 
+def get_data(club, competition):
+    club_data = next((c for c in clubs if c["name"] == club), None)
+    competition_data = next((c for c in competitions if c["name"] == competition), None)
+    return club_data, competition_data
+
+
 @app.route("/book/<club>/<competition>")
-def book(competition, club):
-    found_club = next((c for c in clubs if c["name"] == club), None)
-    found_competition = next(
-        (c for c in competitions if c["name"] == competition), None
-    )
-    if found_club and found_competition:
+def book(club, competition):
+    club_data, competition_data = get_data(club, competition)
+    if club_data and competition_data:
         return render_template(
-            "booking.html", club=found_club, competition=found_competition
+            "booking.html", club=club_data, competition=competition_data
         )
     else:
         flash("Something went wrong - Please try again")
         return render_template("welcome.html", club=club, competitions=competitions)
 
 
+def is_valid(club, competition, places_required):
+    club_points = int(club["points"])
+    places_available = int(competition["numberOfPlaces"])
+    places_already_booked = int(
+        club.get("places_already_booked", {}).get(competition["name"], 0)
+    )
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    competition_date = competition["date"]
+    competition_date_object = datetime.strptime(competition_date, "%Y-%m-%d %H:%M:%S")
+    formatted_date = competition_date_object.strftime("%B %d, %Y, at %H:%M %p")
+
+    if places_required <= 0:
+        return "Please enter a number greater than zero"
+
+    elif current_date > competition_date:
+        return f"This competition already took place on {formatted_date}"
+
+    elif (places_already_booked + places_required) > 12:
+        return f"You cannot book more than 12 places for '{competition['name']}' competition"
+
+    elif places_required > club_points:
+        point_label = "point" if club_points == 1 else "points"
+        return f"Sorry, your club has {club_points} {point_label} left"
+
+    elif places_required > places_available:
+        places_label = "place" if places_available == 1 else "places"
+        return f"Sorry, the competition has {places_available} {places_label} left"
+    return None
+
+
 @app.route("/purchase-places", methods=["POST"])
 def purchase_places():
-    # Find the selected club and competition based on the submitted form data
-    club = [c for c in clubs if c["name"] == request.form["club"]][0]
-    competition = [c for c in competitions if c["name"] == request.form["competition"]][
-        0
-    ]
+    """Get club and competition based on the submitted form data to purchase places"""
+    club, competition = get_data(request.form["club"], request.form["competition"])
 
-    # Get the number of places requested
     places_required = request.form["places"]
     if not places_required:
         flash("Please enter a number")
         return render_template("booking.html", club=club, competition=competition)
 
     places_required = int(places_required)
-    club_points = int(club["points"])
-    places_available = int(competition["numberOfPlaces"])
-
-    places_already_booked = int(
-        club.get("places_already_booked", {}).get(competition["name"], 0)
-    )
-
-    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    competition_date = competition["date"]
-    competition_date_object = datetime.strptime(competition_date, "%Y-%m-%d %H:%M:%S")
-    formatted_date = competition_date_object.strftime("%B %d, %Y, at %H:%M %p")
-
-    error_message = None
-
-    if places_required <= 0:
-        error_message = "Please enter a number greater than zero"
-
-    elif current_date > competition_date:
-        error_message = f"This competition already took place on {formatted_date}"
-
-    elif (places_already_booked + places_required) > 12:
-        error_message = f"You cannot book more than 12 places for '{competition['name']}' competition"
-
-    elif places_required > club_points:
-        point_label = "point" if club_points == 1 else "points"
-        error_message = f"Sorry, your club has {club_points} {point_label} left"
-
-    elif places_required > places_available:
-        places_label = "place" if places_available == 1 else "places"
-        error_message = (
-            f"Sorry, the competition has {places_available} {places_label} left"
-        )
-
+    error_message = is_valid(club, competition, places_required)
     if error_message:
         flash(error_message)
         return render_template("booking.html", club=club, competition=competition)
 
-    club["points"] = str(club_points - places_required)
-    competition["numberOfPlaces"] = str(places_available - places_required)
+    club["points"] = str(int(club["points"]) - places_required)
+    competition["numberOfPlaces"] = str(
+        int(competition["numberOfPlaces"]) - places_required
+    )
 
     if "places_already_booked" not in club:
         club["places_already_booked"] = {}
